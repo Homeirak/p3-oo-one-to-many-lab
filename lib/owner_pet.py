19b0d01650475c1ce839e883868db174af2a6e1c
homeira kalim <homeirakalim@gmail.com>
2025-03-25 16:23:05 -0400

    python one to many lab complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,32 @@
+# owner_pet.py
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+    
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"{pet_type} is not a valid per type")
+        
+        Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise Exceptio("Can only add instances of Pet")
+        
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
